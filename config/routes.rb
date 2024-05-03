0c583ad4b63158976191ef051de2525c61ce0d16
Daniel Tsypkin <daniel.tsypkin@kellogg.northwestern.edu>
2024-05-03 16:26:15 +0000

    continued

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # get("/things", :controller => "things", :action => "index")
   get("/dice", :controller => "dice", :action => "index")
+  get("/companies", :controller => "companies", :action => "index")
+  get("/contacts", :controller => "contacts", :action => "index")
 end
